7b9aac618e431e804bc8e8e8b3f908b01505367b
LuckySushi <susanna.fichter@chasacademy.se>
2021-02-01 19:01:50 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,4 @@
+function confirmBooking() {
+    var bookingInput = document.getElementById("bookingInput")
+    alert("Du har skickat in en bokningsförfrågan på: " + bookingInput.value + " Vi återkommer snart för att bekräfta din bokning!");
+}
